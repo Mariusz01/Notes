d777d1316ccb6449f7ea86596f570627d82a8a50
Mariusz01 <45500049+Mariusz01@users.noreply.github.com>
2020-05-27 21:49:28 +0200

    Dodaj pliki projektów.

---
+++ Notes/Models/Note.cs
@@ -0,0 +1,16 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Notes.Models
+{
+    class Note
+    {
+        [PrimaryKey, AutoIncrement]
+        public int ID { get; set; }
+        public string Text { get; set; }
+        public string Numer { get; set; }
+//        public int Numer { get; set; }
+    }
+}
---
+++ Notes/NoteEntryPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using Notes.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Notes
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class NoteEntryPage : ContentPage
+    {
+        public NoteEntryPage()
+        {
+            InitializeComponent();
+        }
+
+        async void OnSaveButtonClicked(object sender, EventArgs e)
+        {
+            var note = (Note)BindingContext;
+ //           note.Numer = int.Parse(nrEntry.Text);
+            note.Numer = nrEntry.Text;
+            await App.Database.SaveNoteAsync(note);
+            await Navigation.PopAsync();
+        }
+
+        async void OnDeleteButtonClicked(object sender, EventArgs e)
+        {
+            var note = (Note)BindingContext;
+            await App.Database.DeleteNoteAsync(note);
+            await Navigation.PopAsync();
+        }
+    }
+}
